883eb2f26a11277a5b32d7b0da5527b27fbafbc8
Miguel-Hernandez1 <miguel.hernandez01a@gmail.com>
2026-08-06 17:24:21 -0500

    Fade in each page on route change
    key={location.pathname} on <main> remounts the content tree on every
    navigation, triggering animate-page (opacity 0->1, 200ms ease-out).
    Header and Footer stay mounted so they never flash.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Layout/Header'
 import Footer from './components/Layout/Footer'
 import HomePage from './pages/HomePage'
@@ -7,10 +7,12 @@ import ResultsPage from './pages/ResultsPage'
 import AffirmationPage from './pages/AffirmationPage'
 
 export default function App() {
+  const location = useLocation()
+
   return (
     <div className="min-h-screen flex flex-col bg-stone-50">
       <Header />
-      <main className="flex-1">
+      <main key={location.pathname} className="flex-1 animate-page">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/quiz" element={<QuizPage />} />
